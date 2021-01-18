73a46ce91f0932db984124ecf3fd11b02223336a
sbl1996 <sbl1996@126.com>
2021-01-18 14:26:54 +0800

    Add NS

--- tfnas/models/ppnas/genotypes.py
+++ tfnas/models/ppnas/genotypes.py
@@ -2,11 +2,38 @@ from collections import namedtuple
 
 Genotype = namedtuple('Genotype', 'normal')
 
-PP_ResNet_1 = Genotype(
+PP_ResNet_CIFAR10_1 = Genotype(
     normal=[
         (1, 4, 'nor_conv_3x3'),
         (2, 3, 'nor_conv_3x3'),
         (1, 6, 'nor_conv_3x3'),
         (5, 7, 'nor_conv_3x3'),
+    ],
+)
+
+PP_ResNet_CIFAR10_2 = Genotype(
+    normal=[
+        (3, 4, 'nor_conv_3x3'),
+        (1, 5, 'nor_conv_3x3'),
+        (2, 6, 'nor_conv_3x3'),
+        (1, 7, 'nor_conv_3x3'),
+    ],
+)
+
+PP_ResNet_CIFAR10_3 = Genotype(
+    normal=[
+        (1, 3, 'nor_conv_3x3'),
+        (3, 4, 'nor_conv_3x3'),
+        (2, 6, 'nor_conv_3x3'),
+        (2, 7, 'nor_conv_3x3'),
+    ],
+)
+
+PP_ResNet_ImageNet_NS_1 = Genotype(
+    normal=[
+        [(2, 4, 'nor_conv_3x3'), (5, 1, 'nor_conv_3x3'), (6, 3, 'nor_conv_3x3'), (7, 6, 'nor_conv_3x3')],
+        [(2, 1, 'nor_conv_3x3'), (5, 3, 'nor_conv_3x3'), (6, 1, 'nor_conv_3x3'), (7, 4, 'nor_conv_3x3')],
+        [(2, 1, 'nor_conv_3x3'), (5, 3, 'nor_conv_3x3'), (6, 4, 'nor_conv_3x3'), (7, 4, 'nor_conv_3x3')],
+        [(1, 4, 'nor_conv_3x3'), (3, 2, 'nor_conv_3x3'), (6, 4, 'nor_conv_3x3'), (2, 7, 'nor_conv_3x3')]
     ],
 )
